00b8d00a135e9fdc6a69bf6c187f540ac6b8012a
takeshita <shun10910@gmail.com>
2023-12-20 10:22:44 +0900

    contest

---
+++ app/src/main/java/com/example/petsns/ui/contest/ContestViewFragment.java
@@ -0,0 +1,38 @@
+package com.example.petsns.ui.contest;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.petsns.R;
+
+public class ContestViewFragment extends Fragment {
+
+    private ContestViewViewModel mViewModel;
+
+    public static ContestViewFragment newInstance() {
+        return new ContestViewFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contest_view, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ContestViewViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/petsns/ui/contest/ContestViewViewModel.java
@@ -0,0 +1,7 @@
+package com.example.petsns.ui.contest;
+
+import androidx.lifecycle.ViewModel;
+
+public class ContestViewViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
