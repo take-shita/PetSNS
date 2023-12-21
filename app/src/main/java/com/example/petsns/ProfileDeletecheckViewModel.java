5177d47af44b58a3110415ebbeaad7851714274c
yuuto-0002 <cryuu12151215@gmail.com>
2023-12-21 17:46:02 +0900

    tajitsu

---
+++ app/src/main/java/com/example/petsns/ProfileDeletecheckViewModel.java
@@ -0,0 +1,7 @@
+package com.example.petsns;
+
+import androidx.lifecycle.ViewModel;
+
+public class ProfileDeletecheckViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/petsns/profile_deletecheckFragment.java
@@ -0,0 +1,36 @@
+package com.example.petsns;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class profile_deletecheckFragment extends Fragment {
+
+    private ProfileDeletecheckViewModel mViewModel;
+
+    public static profile_deletecheckFragment newInstance() {
+        return new profile_deletecheckFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile_deletecheck, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ProfileDeletecheckViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
