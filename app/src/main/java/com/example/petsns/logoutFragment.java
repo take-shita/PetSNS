d9b5ac0c9fc8bdc887dde85bf4ebb790f73bcfaf
yuuto0228 <20213137@kcska.onmicrosoft.com>
2023-12-21 15:43:02 +0900

    yu

---
+++ app/src/main/java/com/example/petsns/logoutFragment.java
@@ -0,0 +1,36 @@
+package com.example.petsns;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class logoutFragment extends Fragment {
+
+    private LogoutViewModel mViewModel;
+
+    public static logoutFragment newInstance() {
+        return new logoutFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_logout, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(LogoutViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
