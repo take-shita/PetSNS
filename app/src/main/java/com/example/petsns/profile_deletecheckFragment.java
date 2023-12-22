49dfe42843486b3c7b37a42c51c68417214f9e3f
yuuto-0002 <cryuu12151215@gmail.com>
2023-12-22 12:33:58 +0900

    tajitsu

--- app/src/main/java/com/example/petsns/profile_deletecheckFragment.java
+++ app/src/main/java/com/example/petsns/profile_deletecheckFragment.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class profile_deletecheckFragment extends Fragment {
 
@@ -31,6 +33,21 @@ public class profile_deletecheckFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(ProfileDeletecheckViewModel.class);
         // TODO: Use the ViewModel
+    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btnBack = view.findViewById(R.id.btnContestTopBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_navigation_contest_info_to_navigation_contest);
+            }
+        });
+
+
+
     }
 
 }
