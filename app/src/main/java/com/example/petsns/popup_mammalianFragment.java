9d02710103af30bb8ab19dcca816e6fcd7b6e909
yuuto0228 <20213137@kcska.onmicrosoft.com>
2024-01-09 16:37:35 +0900

    yu

--- app/src/main/java/com/example/petsns/popup_mammalianFragment.java
+++ app/src/main/java/com/example/petsns/popup_mammalianFragment.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import android.app.Dialog;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import com.example.petsns.R;
+import android.content.Context;
 public class popup_mammalianFragment extends Fragment {
 
     private PopupMammalianViewModel mViewModel;
@@ -32,5 +35,4 @@ public class popup_mammalianFragment extends Fragment {
         mViewModel = new ViewModelProvider(this).get(PopupMammalianViewModel.class);
         // TODO: Use the ViewModel
     }
-
 }
