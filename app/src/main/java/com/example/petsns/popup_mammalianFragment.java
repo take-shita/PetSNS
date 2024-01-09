3414910b5e39b6fb889d01cdc62b06f900f29d31
naka0430 <nakaibu0430@icloud.com>
2024-01-09 16:02:05 +0900

    d

---
+++ app/src/main/java/com/example/petsns/popup_mammalianFragment.java
@@ -0,0 +1,36 @@
+package com.example.petsns;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class popup_mammalianFragment extends Fragment {
+
+    private PopupMammalianViewModel mViewModel;
+
+    public static popup_mammalianFragment newInstance() {
+        return new popup_mammalianFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_popup_mammalian, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PopupMammalianViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
