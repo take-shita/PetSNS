5d6209fdaa4ecb0e6f345673a33ee1c23c457afe
naka0430 <nakaibu0430@icloud.com>
2024-01-11 15:32:38 +0900

    タグ検索からポップアップ表示

--- app/src/main/java/com/example/petsns/tag_searchFragment.java
+++ app/src/main/java/com/example/petsns/tag_searchFragment.java
@@ -1,6 +1,9 @@
 package com.example.petsns;
 
 import androidx.lifecycle.ViewModelProvider;
+
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -42,6 +45,157 @@ public class tag_searchFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Navigation.findNavController(v).navigate(R.id.action_navigation_tag_search_to_navigation_snstop);
+            }
+        });
+
+        Button bth = view.findViewById(R.id.bth);
+
+        bth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = requireContext();
+                Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.fragment_popup_mammalian);
+
+                ViewGroup.LayoutParams params = dialog.getWindow().getAttributes();
+                params.width = 800; // 幅を変更
+                params.height = 600; // 高さを変更
+                dialog.getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+
+                Button btnClose = dialog.findViewById(R.id.btnno);
+
+                btnClose.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) { dialog.dismiss(); }
+                });
+
+                dialog.show();
+            }
+        });
+        Button btb = view.findViewById(R.id.btb);
+
+        btb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = requireContext();
+                Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.fragment_popup_bird);
+
+                ViewGroup.LayoutParams params = dialog.getWindow().getAttributes();
+                params.width = 800; // 幅を変更
+                params.height = 600; // 高さを変更
+                dialog.getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+
+                Button btnClose = dialog.findViewById(R.id.btnno);
+
+                btnClose.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) { dialog.dismiss(); }
+                });
+
+                dialog.show();
+            }
+        });
+        Button bti = view.findViewById(R.id.bti);
+
+        bti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = requireContext();
+                Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.fragment_popup_insect);
+
+                ViewGroup.LayoutParams params = dialog.getWindow().getAttributes();
+                params.width = 800; // 幅を変更
+                params.height = 600; // 高さを変更
+                dialog.getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+
+                Button btnClose = dialog.findViewById(R.id.btnno);
+
+                btnClose.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) { dialog.dismiss(); }
+                });
+
+                dialog.show();
+            }
+        });
+        Button btr = view.findViewById(R.id.btr);
+
+        btr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = requireContext();
+                Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.fragment_popup_bisexual);
+
+                ViewGroup.LayoutParams params = dialog.getWindow().getAttributes();
+                params.width = 800; // 幅を変更
+                params.height = 600; // 高さを変更
+                dialog.getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+
+                Button btnClose = dialog.findViewById(R.id.btnno);
+
+                btnClose.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) { dialog.dismiss(); }
+                });
+
+                dialog.show();
+            }
+        });
+        Button bts = view.findViewById(R.id.bts);
+
+        bts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = requireContext();
+                Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.fragment_popup_aquatic);
+
+                ViewGroup.LayoutParams params = dialog.getWindow().getAttributes();
+                params.width = 800; // 幅を変更
+                params.height = 600; // 高さを変更
+                dialog.getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+
+                Button btnClose = dialog.findViewById(R.id.btnno);
+
+                btnClose.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) { dialog.dismiss(); }
+                });
+
+                dialog.show();
+            }
+        });
+        Button btha = view.findViewById(R.id.btha);
+
+        btha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = requireContext();
+                Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.fragment_popup_reptiles);
+
+                ViewGroup.LayoutParams params = dialog.getWindow().getAttributes();
+                params.width = 800; // 幅を変更
+                params.height = 600; // 高さを変更
+                dialog.getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+
+                Button btnClose = dialog.findViewById(R.id.btnno);
+
+                btnClose.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) { dialog.dismiss(); }
+                });
+
+                dialog.show();
             }
         });
 
