43810d3e574ceed2fd949a6a5b202b6bcee2ea4a
naka0430 <nakaibu0430@icloud.com>
2024-01-12 16:29:07 +0900

    ｈｇｈｇｈ

--- app/src/main/java/com/example/petsns/ui/snstop/snstopFragment.java
+++ app/src/main/java/com/example/petsns/ui/snstop/snstopFragment.java
@@ -2,28 +2,19 @@ package com.example.petsns.ui.snstop;
 
 import androidx.lifecycle.ViewModelProvider;
 
-import android.app.Dialog;
-import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.petsns.R;
-import com.example.petsns.MyAdapter;
-import com.example.petsns.ui.setting.TagDislikeFragment;
-import com.example.petsns.ui.setting.TagLikeFragment;
-import com.google.android.material.tabs.TabLayout;
 
-import android.widget.Button;
 import android.widget.ImageButton;
 
 public class snstopFragment extends Fragment {
@@ -34,25 +25,25 @@ public class snstopFragment extends Fragment {
         return new snstopFragment();
     }
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+//                             @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_snstop, container, false);
-
-        // RecyclerViewを取得
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
-
-        // レイアウトマネージャーを設定
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(layoutManager);
-
-        // アダプターを設定（自分のアダプタークラスに置き換える必要があります）
-        MyAdapter adapter = new MyAdapter();
-        recyclerView.setAdapter(adapter);
-
-        return view;
-    }
+//        View view = inflater.inflate(R.layout.fragment_snstop, container, false);
+//
+//        // RecyclerViewを取得
+//        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+//
+//        // レイアウトマネージャーを設定
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+//        recyclerView.setLayoutManager(layoutManager);
+//
+//        // アダプターを設定（自分のアダプタークラスに置き換える必要があります）
+//        MyAdapter adapter = new MyAdapter();
+//        recyclerView.setAdapter(adapter);
+//
+//        return view;
+//    }
 
 
     @Override
