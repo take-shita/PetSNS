7514f36e518d266e11af9f05ee3a62d0ddcdab99
rukia <rukia7640@icloud.com>
2024-01-12 10:42:35 +0900

    ru

--- app/src/main/java/com/example/petsns/ui/setting/pass/pass1Fragment.java
+++ app/src/main/java/com/example/petsns/ui/setting/pass/pass1Fragment.java
@@ -4,15 +4,19 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
+import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+
 import com.example.petsns.R;
 
 public class pass1Fragment extends Fragment {
@@ -42,14 +46,29 @@ public class pass1Fragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         Button bt01 = view.findViewById(R.id.bt01);
+        EditText passwordField = view.findViewById(R.id.passwordField);
 
         bt01.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Navigation.findNavController(v).navigate((R.id.action_navigation_pass1_to_navigation_phone));
+
+                String enteredPassword = passwordField.getText().toString();
+
+                // パスワードが 8 文字以上であることを確認
+                if (TextUtils.isEmpty(enteredPassword) || enteredPassword.length() < 8) {
+                    showToast("パスワードは 8 文字以上で入力してください");
+                } else {
+                    Navigation.findNavController(v).navigate((R.id.action_navigation_pass1_to_navigation_phone));
+                }
+            }
+
+            private void showToast(String s) {
             }
         });
+        
+        
+        
         Button bt02 = view.findViewById(R.id.bt02);
 
         bt02.setOnClickListener(new View.OnClickListener() {
