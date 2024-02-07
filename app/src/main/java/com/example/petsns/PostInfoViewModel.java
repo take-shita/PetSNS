0744d041787eb0ccebfaee026eda158262b36a8b
rukia <rukia7640@icloud.com>
2024-02-07 14:17:33 +0900

    ;lkjhg

--- app/src/main/java/com/example/petsns/PostInfoViewModel.java
+++ app/src/main/java/com/example/petsns/PostInfoViewModel.java
@@ -1,12 +1,31 @@
 package com.example.petsns;
 
+import android.net.Uri;
+
 import androidx.lifecycle.ViewModel;
 
 public class PostInfoViewModel extends ViewModel {
-    private String sentence="sample";
+    private String sentence=null;
+    private Uri image=null;
 
-
+    public void setSentence(String sentence){
+        this.sentence=sentence;
+    }
     public String getSentence(){
         return this.sentence;
     }
+
+    public void setImage(Uri image){
+        this.image=image;
+    }
+    public Uri getImage(){
+        return image;
+    }
+
+    public void postCancel(){
+        sentence=null;
+        image=null;
+    }
+
+
 }
